c79cdd41a6456fbf3938c1f05d055ac4270fb1dd
Silvio Monte <70415429+devsmonte@users.noreply.github.com>
2023-08-30 14:55:27 -0300

    Create 30 atividade.js

---
+++ 30 atividade.js
@@ -0,0 +1,13 @@
+//função tradicional com palavra reservada function
+function soma (){}
+
+//função tradicional com parametros e retorno
+function soma2 (num1, num2){
+    return num1 + num2
+}
+
+//arrow function com parametros e retorno.
+const soma3 = (num1, num2) => num1 + num2
+
+
+
