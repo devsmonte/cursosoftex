efd4f09ceac8459a4d2f1878f6084f5b1a04e05b
Silvio Monte <70415429+devsmonte@users.noreply.github.com>
2023-09-02 22:14:36 -0300

    Create 32 atividade.js

---
+++ 32 atividade.js
@@ -0,0 +1,39 @@
+
+
+let pessoas = [
+  {
+    "nome": "joao",
+    "idade": 20,
+    "profissao": "professor",
+    "cidade":"recife"
+  },
+    {
+    "nome": "maria",
+    "idade": 22,
+    "profissao": "auxiliar",
+    "cidade":"jaboatao"
+  },
+    {
+    "nome": "jose",
+    "idade": 30,
+    "profissao": "motorista",
+    "cidade":"cabo"
+  },
+    {
+    "nome": "ana",
+    "idade": 31,
+    "profissao": "enfermeira",
+    "cidade":"paulista"
+  }]
+
+
+for (let x in pessoas){
+console.log(x);
+}
+
+
+for (let y of pessoas){
+    console.log(y);
+}
+
+
